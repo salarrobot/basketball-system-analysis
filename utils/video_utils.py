be41d7e052656ad64c539c14454e87de495f585e
salarrobot <mokhtarilalehs@gmail.com>
2025-06-09 21:14:32 -0500

    update 1.1.1

---
+++ utils/video_utils.py
@@ -0,0 +1,17 @@
+import os 
+import cv2
+
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+
+    cap.release()
+    return frames
+
+def save_video(frames: list, output_path):
