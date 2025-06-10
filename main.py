a77f84fb7654d6776cdf3a0a3f080b73f286e57f
salarrobot <mokhtarilalehs@gmail.com>
2025-06-09 22:01:33 -0500

    update Trackers: Player_Tracker

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from utils import read_video, save_video
+from trackers import PlayerTracker
+
 
 def main():
 
     video_frames = read_video("input_videos/video_1.mp4")
+
+    player_tracker = PlayerTracker("models/player_detector.pt")
+
+    player_tracks = player_tracker.get_object_tracks(video_frames)
+
+    print("player_tracks")
+    
     save_video(video_frames, "output_videos/output_video.avi")
 
 if __name__== "__main__":
