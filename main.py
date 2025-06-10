e5cff5f220b165419cef0c97813d5d775d923214
salarrobot <mokhtarilalehs@gmail.com>
2025-06-09 21:03:29 -0500

    update 1.1.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8m')
+model = YOLO('/home/ntnu/Desktop/ml/basketball-system-analysis/models/player_detector.pt')
 
 results = model.predict('input_videos/video_1.mp4', save=True)
 print(results)
